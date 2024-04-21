99075e206f4cdbf5c30728d351bc1ab46f621f0e
Smit Radadiya <smit.radadiya@nielsen.com>
2024-04-21 14:43:48 -0300

    resolved some issues

--- src/app/contactus/page.tsx
+++ src/app/contactus/page.tsx
@@ -8,7 +8,7 @@ const page = () => {
             <div className='form-container'>
                 <div className='form-inner-container'>
                     {/* <div style={{filter: "hue-rotate(189.73deg) saturate(18.61%) brightness(96.86%)"}}> */}
-                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd46svSrMMTpQsc52pQbhFNacI_-v_8ySIK986t-dmZVj1L_A/viewform?embedded=true" frameborder="0" marginheight="0" marginwidth="0" height={"100%"} scrolling="no"
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd46svSrMMTpQsc52pQbhFNacI_-v_8ySIK986t-dmZVj1L_A/viewform?embedded=true" height={"100%"} scrolling="no"
                         style={{ width: "100%" }} className="ifram-cls"
                     >Loading…</iframe>
                     {/* </div> */}
