06f6f408b528e7befc0b0b37cb1a5f17b1d902e3
Smit Radadiya <smit.radadiya@nielsen.com>
2024-04-25 20:50:58 -0300

    removed bgcolor overriden

--- src/components/utils/LinkWrapper/LinksWrapper.tsx
+++ src/components/utils/LinkWrapper/LinksWrapper.tsx
@@ -6,7 +6,7 @@ import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import './linkwrapper.scss'
 const LinksWrapper = () => {
   return (
-    <a href="#" className="inline-flex items-center justify-center p-5 text-base font-medium text-white rounded-lg bg-slate-50 hover:text-gray-900 hover:bg-gray-100 dark:text-white-400 dark:bg-cyan-950 dark:hover:bg-gray-700 dark:hover:text-white link-wrapper">
+    <a href="#" className="inline-flex items-center justify-center p-5 text-base font-medium text-white rounded-lg  hover:text-gray-900 hover:bg-gray-100 dark:text-white-400 dark:bg-cyan-950 dark:hover:bg-gray-700 dark:hover:text-white link-wrapper">
       {/* <Image src={icon.src} /> */}
       <InfoIcon />
       <span className="w-full pl-3 pr-1 btn-txt">Get Started With Your MotorCycle Journey In Nova Scotia</span>
